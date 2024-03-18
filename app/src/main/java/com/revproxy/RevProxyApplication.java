d203fad582d3ec27ef738d2d0cc17565d172144e
JM <jmnavarro@gmail.com>
2024-03-18 20:56:09 +0100

    add missing annotation

--- app/src/main/java/com/revproxy/RevProxyApplication.java
+++ app/src/main/java/com/revproxy/RevProxyApplication.java
@@ -2,8 +2,10 @@ package com.revproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class RevProxyApplication {
 
 	public static void main(String[] args) {
