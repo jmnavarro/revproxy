68aeae5df8f7c18b48c395520609a2f193dba656
JM <jmnavarro@gmail.com>
2024-03-18 23:11:20 +0100

    Support all HTTP methods

--- app/src/main/java/com/revproxy/controller/RevProxyController.java
+++ app/src/main/java/com/revproxy/controller/RevProxyController.java
@@ -34,24 +34,70 @@ public class RevProxyController {
     @GetMapping
     public Mono<ResponseEntity<Object>> get(@NonNull @RequestParam Map<String, String> params,
                                             @NonNull @RequestHeader Map<String, String> headers) {
-
-        // standarize header keys
-        final var lowerCasedHeaders = convertHeadersToLowercase(headers);
-
         // There's some literature around the semantics of the body in GET requests.
         // Supporting body in this context doesn't do any harm, while it makes the server more
         // flexible, so it seems to be a good idea to support it.
+        return handleRequest(HttpMethod.GET, params, headers, Optional.empty());
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<Object>> post(@NonNull @RequestParam Map<String, String> params,
+                                             @NonNull @RequestHeader Map<String, String> headers,
+                                             @RequestBody(required = false) Object body) {
+        return handleRequest(HttpMethod.POST, params, headers, Optional.ofNullable(body));
+    }
+
+    @PutMapping
+    public Mono<ResponseEntity<Object>> put(@NonNull @RequestParam Map<String, String> params,
+                                            @NonNull @RequestHeader Map<String, String> headers,
+                                            @RequestBody(required = false) Object body) {
+        return handleRequest(HttpMethod.PUT, params, headers, Optional.ofNullable(body));
+    }
+
+    @PatchMapping
+    public Mono<ResponseEntity<Object>> patch(@NonNull @RequestParam Map<String, String> params,
+                                              @NonNull @RequestHeader Map<String, String> headers,
+                                              @RequestBody(required = false) Object body) {
+        return handleRequest(HttpMethod.PATCH, params, headers, Optional.ofNullable(body));
+    }
+
+    @DeleteMapping
+    public Mono<ResponseEntity<Object>> delete(@NonNull @RequestParam Map<String, String> params,
+                                               @NonNull @RequestHeader Map<String, String> headers,
+                                               @RequestBody(required = false) Object body) {
+        return handleRequest(HttpMethod.DELETE, params, headers, Optional.ofNullable(body));
+    }
+
+    @RequestMapping(method=RequestMethod.OPTIONS)
+    public Mono<ResponseEntity<Object>> options(@NonNull @RequestParam Map<String, String> params,
+                                                @NonNull @RequestHeader Map<String, String> headers) {
+        return handleRequest(HttpMethod.OPTIONS, params, headers, Optional.empty());
+    }
+
+    @RequestMapping(method=RequestMethod.HEAD)
+    public Mono<ResponseEntity<Object>> head(@NonNull @RequestParam Map<String, String> params,
+                                             @NonNull @RequestHeader Map<String, String> headers) {
+        return handleRequest(HttpMethod.HEAD, params, headers, Optional.empty());
+    }
+
+    private Mono<ResponseEntity<Object>> handleRequest(@NonNull HttpMethod method,
+                                                       @NonNull Map<String, String> params,
+                                                       @NonNull Map<String, String> headers,
+                                                       Optional<Object> body) {
+        // standarize header keys
+        final var lowerCasedHeaders = toLowercaseHeaders(headers);
+
         return ReactiveRequestContextHolder.getRequest()
                 .map(ServerHttpRequest::getURI)
-                .map(path -> new ProxyRequest(HttpMethod.GET, path.getScheme(), path.getPath(), params, lowerCasedHeaders, Optional.empty()))
+                .map(path -> new ProxyRequest(method, path.getScheme(), path.getPath(), params, lowerCasedHeaders, body))
                 .flatMap(proxyService::send)
                 .map(this::processResult);
     }
 
-    private Map<String, String> convertHeadersToLowercase(Map<String, String> headers) {
+    private Map<String, String> toLowercaseHeaders(Map<String, String> headers) {
         return headers.entrySet().stream().map(
                 entry -> new AbstractMap.SimpleEntry<>(entry.getKey().toLowerCase(), entry.getValue())
-        ).collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
+        ).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     private ResponseEntity<Object> processResult(@NonNull ProxyResponse result) {
