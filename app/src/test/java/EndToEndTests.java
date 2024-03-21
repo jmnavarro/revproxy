30fa7491a8fc907f8ef89b8b03e7d1818e15ed49
JM <jmnavarro@gmail.com>
2024-03-22 00:55:11 +0100

    Add end to end test to validate timeout and retries

--- app/src/test/java/EndToEndTests.java
+++ app/src/test/java/EndToEndTests.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.Instant;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.util.AssertionErrors.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -29,4 +32,25 @@ public class EndToEndTests {
         assertNotNull("Should receive a response", response);
         assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
     }
+
+    @Test
+    public void timeoutAndRetriesShouldWork() {
+        int retries = 2;
+        int timeout = 1 * 1000;
+        int maxTime = timeout * (retries + 1);
+
+        long startTime = Instant.now().toEpochMilli();
+
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+        ResponseEntity<String> response = testRestTemplate.getForEntity( "http://localhost:" + port + "/delay/5", String.class);
+
+        long timeElapsed = Instant.now().toEpochMilli() - startTime;
+
+        assertTrue(String.format("Took %d ms but should take at least %d ms", timeElapsed, maxTime),
+                timeElapsed >= maxTime);
+
+        assertNotNull("Should receive a response", response);
+        assertEquals(HttpStatusCode.valueOf(504), response.getStatusCode());
+    }
+
 }
